11711f1842b5e938c6ab998022d3685333918c81
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2018-02-05 09:23:09 -0500

    asd

---
+++ mergepartitionlab/src/mergepartitionlab/ArrayMethods2.java
@@ -0,0 +1,24 @@
+/*
+*  Author: Sophia Qiu
+*  Period 2
+*  Date Created: 01.17.2018
+*  Project Name: Lab 3.1 In Place Sorts
+*/
+
+package mergepartitionlab;
+
+public class ArrayMethods2 {
+	
+	public static String[] merge(String[] list1, String[] list2)
+	{
+		int[] list1 = new int[] {4, 7, 1, 1, 9, 2, 5};
+		
+	}
+	
+	
+	public static int partition(int[] list)
+	{
+		
+	}
+	
+}
